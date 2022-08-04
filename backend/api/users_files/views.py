fc1ccc6c900b0ecc9cc31e8b0ed34ca78e0959e2
Кирилл Косоротов <ampolirostinvest@yandex.ru>
2022-08-04 17:17:45 +0300

    Деплой v4

--- backend/api/users_files/views.py
+++ backend/api/users_files/views.py
@@ -61,7 +61,7 @@ class SubscriptionCreateDestroyView(APIView):
             return Response(
                 {'errors': 'Вы уже подписаны на данного пользователя'},
                 status=HTTP_400_BAD_REQUEST)
-        #if serializer.is_valid(raise_exception=True):
+        # if serializer.is_valid(raise_exception=True):
         serializer.save()
         return Response(serializer.data, status=HTTP_201_CREATED)
 
