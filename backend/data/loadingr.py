67e00fdd55a2828d0259bfbe43eb43b3165620c1
Кирилл Косоротов <ampolirostinvest@yandex.ru>
2022-08-09 16:57:39 +0300

    Деплой v15

---
+++ backend/data/loadingr.py
@@ -0,0 +1,22 @@
+import json
+
+file_name='ingredients'
+pk = 0
+new_list = []
+#"./data/ingredients.json", 'r'
+with open("./data/ingredients.json", 'r') as json_data:
+    d = json.load(json_data)    
+    for item in d:
+        pk+=1
+        item = {"model": "recipes.ingredient", "pk": pk, "fields": item}
+        new_list.append(item)
+        json_data.close()
+
+
+def list_to_json_file(list_of_dicts, file_name):
+    with open(file_name+'.json', 'w') as file:
+        json.dump(list_of_dicts, file, ensure_ascii=False)
+    print('{}.Json file created'.format(file_name))
+
+
+list_to_json_file(new_list, 'JSON_for_django')
